1ae21491980b526a9a1d4bc6ac6b45b225618817
srivyshnaviguvvala29 <vyshnavilucky001@gmail.com>
2026-08-14 11:31:28 +0530

    Add Calculator class with unit testing methods
    Implement basic calculator functionality with unit testing.

---
+++ TASK9:CalculatorUnitTesting.java
@@ -0,0 +1,36 @@
+#TASK9: Calculator Unit Testing
+##SOUCE CODE:
+import java.util.*;
+public class Calculator {
+    static int add(int a,int b){
+        return a+b;
+    }
+    static int divide(int a,int b){
+        return a / b;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String operator = sc.next();
+        int b = sc.nextInt();
+        boolean passed = false;
+        if(operator.equals("+")){
+            passed = (add(a,b) == a+b);
+} 
+        else if (operator.equals("/")) {
+            if (b != 0) {
+                passed = (divide(a, b) == a / b);
+            }
+        }
+
+        if (passed) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
+    }
+}
+##SMAPLE INPUT
+  10 + 20
+##SAMPLE OUTPUT
+  Test Passed
