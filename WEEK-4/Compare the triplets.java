754f7e6f4e3ecedd038c60813721632bc87d6b19
srivyshnaviguvvala29 <vyshnavilucky001@gmail.com>
2026-09-10 09:43:29 +0530

    Create Compare the triplets.java

---
+++ WEEK-4/Compare the triplets.java
@@ -0,0 +1,27 @@
+public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+    
+    int aliceScore = 0;
+    int bobScore = 0;
+    
+    for (int i = 0; i < 3; i++) {
+        
+        if (a.get(i) > b.get(i)) {
+            aliceScore++;
+        } 
+        else if (a.get(i) < b.get(i)) {
+            bobScore++;
+        }
+    }
+    
+    List<Integer> result = new ArrayList<>();
+    result.add(aliceScore);
+    result.add(bobScore);
+    
+    return result;
+}
+
+  ##sample input
+  17 28 30
+  99 16 8
+  ##sample output
+  2 1
