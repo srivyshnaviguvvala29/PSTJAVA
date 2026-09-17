681fbfaafe473d3b2d58e783b033e0055ebda828
srivyshnaviguvvala29 <vyshnavilucky001@gmail.com>
2026-09-17 14:10:17 +0530

    Create Design hashset.java

---
+++ WEEK-7/Design hashset.java
@@ -0,0 +1,25 @@
+class MyHashSet {
+
+    private boolean[] set;
+
+    public MyHashSet() {
+        set = new boolean[1000001];
+    }
+
+    public void add(int key) {
+        set[key] = true;
+    }
+
+    public void remove(int key) {
+        set[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+Input
+["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+Output
+[null, null, null, true, false, null, true, null, false]
